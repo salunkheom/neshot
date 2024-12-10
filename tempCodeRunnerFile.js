da3ebe7ef7eb130de746055febaf48aecfe3c3e9
Omi <121783352+salunkheom@users.noreply.github.com>
2024-12-10 19:45:45 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,36 @@
+// Initialize a new Swiper instance for the element with the class "mySwiper"
+// var swiper = new Swiper(".mySwiper", {
+//   effect: "coverflow", // Set the effect to 'coverflow' for a 3D coverflow style
+//   grabCursor: true, // Change the cursor to a grab cursor when hovering over the swiper
+//   centeredSlides: true, // Center the active slide in the viewport
+//   slidesPerView: "auto", // Automatically determine the number of slides to show based on their width
+
+//   slidesPerView: 10, // Set the default number of slides to show to 10
+//   spaceBetween: 5, // Set the space between slides to 5 pixels
+
+//   // Pagination settings
+//   pagination: {
+//     el: ".swiper-pagination", // Specify the element for pagination
+//     clickable: true, // Allow pagination bullets to be clickable
+//   },
+
+//   // Responsive breakpoints for different screen sizes
+//   breakpoints: {
+//     "@0.00": { // For screens smaller than 0.00 (mobile)
+//       slidesPerView: 1, // Show 1 slide
+//       spaceBetween: 10, // Space between slides is 10 pixels
+//     },
+//     "@0.75": { // For screens 0.75 and larger (small tablets)
+//       slidesPerView: 2, // Show 2 slides
+//       spaceBetween: 20, // Space between slides is 20 pixels
+//     },
+//     "@1.00": { // For screens 1.00 and larger (tablets)
+//       slidesPerView: 3, // Show 3 slides
+//       spaceBetween: 40, // Space between slides is 40 pixels
+//     },
+//     "@1.50": { // For screens 1.50 and larger (desktops)
+//       slidesPerView: 4, // Show 4 slides
+//       spaceBetween: 50, // Space between slides is 50 pixels
+//     },
+//   },
+// });
